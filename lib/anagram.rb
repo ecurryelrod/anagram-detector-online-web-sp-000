0b61be30ecdba0f38a80a7db85b2e70906b722df
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-04-25 22:21:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,15 @@
 # Your code goes here!
 
 class Anagram
-  attr_accessor :words 
+  attr_accessor :word
   
-  def initialize(words)
-    @words = words
+  def initialize(word)
+    @word = word
   end 
   
   def match(words)
     words.select do |word| 
-      word.split("").sort == @words.split("").sort
+      word.split("").sort == @word.split("").sort
     end
   end 
 end
